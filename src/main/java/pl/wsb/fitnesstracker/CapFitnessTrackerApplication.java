217449eaeb78f7e659931f5f69e049786c2966cf
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-05-09 10:06:44 +0200

    lab03-entities2

---
+++ src/main/java/pl/wsb/fitnesstracker/CapFitnessTrackerApplication.java
@@ -0,0 +1,12 @@
+package pl.wsb.fitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapFitnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapFitnessTrackerApplication.class, args);
+    }
+}
